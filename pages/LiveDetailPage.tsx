ac6b9b26bdfb3a9a03dbef41aa66bfa9868d4f91
untukai <ai2024untuk@gmail.com>
2025-11-15 18:56:39 +0700

    refactor: Remove unused trailing newlines
    Removes unnecessary blank lines at the end of several page component files. This is a minor cleanup to maintain code hygiene.

--- pages/LiveDetailPage.tsx
+++ pages/LiveDetailPage.tsx
@@ -1,5 +1,4 @@
 
-
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { liveSessions, sellers, products, endLiveSession } from '../data/dummyData';
